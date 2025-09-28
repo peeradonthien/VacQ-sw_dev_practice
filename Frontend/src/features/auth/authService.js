65cb8f4716abf9aaa95c78abaddb03621a7e589a
Peeradon <peeradonthienpongkasem@gmail.com>
2025-09-28 19:17:26 +0700

    finish login system

--- Frontend/src/features/auth/authService.js
+++ Frontend/src/features/auth/authService.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api/v1/auth/register'
+const API_URL = 'http://localhost:5000/api/v1/auth/'
 
 //Register user
 const register = async (userData) => {
     try{
-        const response = await axios.post(API_URL,userData);
+        const response = await axios.post(API_URL+'register/',userData);
         console.log(JSON.stringify(response.data));
         console.log(response.data.name);
         if(response.data){
@@ -20,8 +20,25 @@ const register = async (userData) => {
 
 }
 
+//Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL+'login',userData);
+    if(response.data){
+        localStorage.setItem('user',response.data.name)
+    }
+    console.log(response.data);
+    return response.data
+}
+
+//Logout user
+const logout = () =>{
+    localStorage.setItem('user',null);
+}
+
 const authService ={
     register,
+    logout,
+    login
 }
 
 export default authService;
--- Frontend/src/features/auth/authSlice.js
+++ Frontend/src/features/auth/authSlice.js
@@ -1,6 +1,7 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import authService from './authService'
 
+
 //Get user from localstorage
 //const user = JSON.parse(localStorage.getItem('user'))
 const user=localStorage.getItem('user');
@@ -26,8 +27,18 @@ export const register = createAsyncThunk('auth/register',async(user,thunkAPI)=>{
 
 //Login user
 export const login = createAsyncThunk('auth/login',async(user,thunkAPI)=>{
-    console.log(user);
+    try{
+        return await authService.login(user)
+    }catch(error){
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+        return thunkAPI.rejectWithValue(message)
+    }
 });
+
+//Logout user
+export const logout = createAsyncThunk('auth/logout',async()=>{
+    await authService.logout()
+})
 
 export const authSlice = createSlice({
     name: 'auth',
@@ -54,6 +65,23 @@ export const authSlice = createSlice({
             state.isLoading = false
             state.isError = true
             state.message = action.payload
+            state.user = null
+        })
+        .addCase(login.pending, (state)=>{
+            state.isLoading=true
+        })
+        .addCase(login.fulfilled, (state,action)=>{
+            state.isLoading = false
+            state.isSuccess = true
+            state.user=action.payload
+        })
+        .addCase(login.rejected,(state,action)=>{
+            state.isLoading = false
+            state.isError = true
+            state.message = action.payload
+            state.user = null
+        })
+        .addCase(logout.fulfilled, (state)=>{
             state.user = null
         })
     }
